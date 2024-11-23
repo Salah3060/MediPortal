3e1c929ab0259a656ea2c20e87b12a2ac305a6d7
Ahmed Kamal <ahmd675asd@gmail.com>
2024-11-23 20:49:42 +0200

    Front File Structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#253237",
+        secondary: "#5c6b73",
+        darkRed: "#ae0d0d",
+        darkGrayText: "#666666",
+        lightGrayText: "#7c8a9d",
+      },
+    },
   },
   plugins: [],
 };
