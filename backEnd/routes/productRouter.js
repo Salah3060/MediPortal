9d738ad515fbff86c705a3530748f1a5df21a519
Ahmed Kamal <ahmd675asd@gmail.com>
2024-12-04 20:01:04 +0200

    n

--- backEnd/routes/productRouter.js
+++ backEnd/routes/productRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getAllProducts } from "../controllers/productController.js";
+
 const router = express.Router();
 
 router.get("/allProducts", getAllProducts);
