61c1a0692d09b1ad91facc3f766bcb44978d19ef
Salah_Mostafa <salahmostafa3060@gamil.com>
2024-12-13 18:08:25 +0200

    BE solve bug

--- backEnd/routes/providerRouter.js
+++ backEnd/routes/providerRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { getAllProviders } from "../controllers/providerController.js";
+import { validateLoggedIn, restrictTo } from "../controllers/authController.js";
 const router = express.Router();
 
 router.use(validateLoggedIn, restrictTo("Admin"));
-router.get("/allInsurances", getAllInsurances);
 router.get("/allProviders", getAllProviders);
 
 export default router;
