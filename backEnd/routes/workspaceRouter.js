b42ee08db0a43ea191bcdaa400e0743175431239
Salah_Mostafa <salahmostafa3060@gamil.com>
2024-12-22 18:14:42 +0200

    BE enhance

--- backEnd/routes/workspaceRouter.js
+++ backEnd/routes/workspaceRouter.js
@@ -6,11 +6,11 @@ import {
   getAllWorkSpaces,
   getAllLocations,
 } from "../controllers/workspaceController.js";
-import { restrictTo } from "../controllers/authController.js";
+import { restrictTo, validateLoggedIn } from "../controllers/authController.js";
 
 const router = express.Router();
 router.get("/allWorkSpaces", getAllWorkSpaces);
-
+router.use(validateLoggedIn);
 router.use(restrictTo("Admin", "Doctor"));
 router.post("/", createWorkspace);
 router
